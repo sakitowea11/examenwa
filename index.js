8e94d0831557d0a82dea7efc0d7ebd268d34afeb
Cristopher Rugel <carugel1@espe.edu.ec.com>
2022-01-24 15:57:06 -0500

    Puerto

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const hbs = require('hbs');
 
 require('./hbs/helpers');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname+'/public'));
 
